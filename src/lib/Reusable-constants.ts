fb53b6849fda2befe8f3f8afe1243bc5f57de4f5
goswami-rohit <rohit.second44@gmail.com>
2026-05-13 17:06:47 +0530

    eurofoam dashboard basic layout made3

--- src/lib/Reusable-constants.ts
+++ src/lib/Reusable-constants.ts
@@ -5,18 +5,16 @@ export const BASE_URL = process.env.NEXT_PUBLIC_APP_URL;
 export const JWT_KEY = process.env.JWT_SECRET;
 
 export const brands = [
-  "Star", "Amrit", "Dalmia", 
-  "Topcem", "Black Tiger", "Surya Gold", 
-  "Max", "Taj", 
+  "Kurlon", "Sleepwell", "Wakefit", "The Sleep Company", "Sleepy Cat", "Duroflex", "Flo Mattress",
   "Specify in remarks"
 ];
 
-export const dealerTypes = ["Dealer-Best", "Sub Dealer-Best", "Dealer-Non Best", "Sub Dealer-Non Best"];
+export const dealerTypes = ["Dealer-Eurofoam", "Dealer-Non Eurofoam"];
 
-export const units = ["MT", "KG", "Bags"];
+export const units = ["KG"];
 
-export const Zone = ["All Zone", "Kamrup", "Upper Assam", "Lower Assam", "Central Assam", 
-  "Barak Valley", "North Bank", "Meghalaya", "Mizoram", "Nagaland", "Tripura", 
+export const Zone = ["All Zone", "Lower Assam 1", "Lower Assam 2", "Central Assam", "Upper Assam",
+  "Barak Valley", "North Bank 1", "North Bank 2", "Meghalaya", "Nagaland", "Tripura", "Other",
 ];
 
 export const SO_AOP_TARGETS = {
